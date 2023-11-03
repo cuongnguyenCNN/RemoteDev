10a7fcd6d48ce718b9d96e9e1549dc17eef67276
Nguyễn Ngọc Cương <cuongnguyen71195@gmail.com>
2023-11-03 20:11:09 +0700

    Initial Project

---
+++ src/App.js
@@ -0,0 +1,210 @@
+import logo from './logo.svg';
+import './App.css';
+//import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+  return (
+    <div className="App">
+      <div class="top">
+        <div class="remote-ok-logo">
+          <h1>
+            <a class="no-border" href="/">
+              <span class="pacifico">
+                R<span class="hide-before-1000px">emote</span>
+              </span>
+            </a>
+            <a class="no-border" href="/">
+              <span class="ok">
+                DEV<span class="trademark">®</span>
+              </span>
+            </a>
+          </h1>
+        </div>
+        <div class="box">
+          <h1>
+            <span class="hide-before-300px">find</span> a{" "}
+            <b>
+              <span class="hide-before-400px">remote </span>job
+            </b>
+            <br />
+            <span class="hide-before-800px">
+              work from <b>anywhere</b>
+            </span>
+          </h1>
+
+          <input
+            class="search"
+            autocomplete="new-password"
+            type="text"
+            placeholder="your next job?"
+          />
+        </div>
+      </div>
+      <div class="top-right-nav">
+        <span class="ad advertisement sponsored" rel="sponsored">
+          <a
+            class="button remote-health"
+            rel="sponsored"
+            href="/nomad-health-individual"
+          >
+            Health insurance
+          </a>
+        </span>
+        <a
+          class="button action-post-job"
+          href="/hire-remotely?discount_code=a33f0de8672af573c67e182c72654b41&amp;ref=top"
+        >
+          Post <span class="hide-before-600px">a remote </span>job{" "}
+        </a>
+        <a class="button white action-log-in" href="/login">
+          Log in
+        </a>
+      </div>
+      <container>
+        <table id="jobsboard">
+          <tbody>
+            <tr
+              id="job-109466"
+              data-id="109466"
+              class="job job-109466 verified  hot has-highlight-color  sticky bumped 												"
+              title=""
+            >
+              <td class="image has-logo">
+                <a
+                  alt="IAPWE"
+                  class="preventLink"
+                  href="/remote-jobs/remote-freelance-writer-iapwe-109466"
+                >
+                  <img
+                    alt="IAPWE"
+                    data-z="1"
+                    data-extension="png"
+                    itemprop="image"
+                    class="logo lazy lazyloaded"
+                    src="https://remoteok.com/cdn-cgi/image/format=auto,fit=contain,width=100,height=100,quality=50/https://remoteOK.com/assets/img/jobs/f965d5c27e5954dbfffa10ecdbcd36331646841248.png?1646841248"
+                    data-src="https://remoteok.com/cdn-cgi/image/format=auto,fit=contain,width=100,height=100,quality=50/https://remoteOK.com/assets/img/jobs/f965d5c27e5954dbfffa10ecdbcd36331646841248.png?1646841248"
+                  />
+                </a>
+              </td>
+              <td class="company position company_and_position">
+                <a
+                  itemprop="url"
+                  class="preventLink"
+                  href="/remote-jobs/remote-freelance-writer-iapwe-109466"
+                >
+                  <h2 itemprop="title">Freelance Writer</h2>
+                </a>
+                <span class="original tooltip-set" title="">
+                  🎈
+                </span>{" "}
+                <span class="verified tooltip-set" title="">
+                  verified
+                </span>
+                <br />
+                <span
+                  itemprop="hiringOrganization"
+                  itemscope=""
+                  itemtype="http://schema.org/Organization"
+                  class="companyLink"
+                  href="/iapwe"
+                >
+                  <h3 itemprop="name">IAPWE </h3>
+                  &nbsp;
+                  <img
+                    alt="This job is getting a high amount of applications right now (15% of viewers clicked Apply)"
+                    src="https://remoteok.com/assets/hot2x.gif"
+                    width="28"
+                    height="11"
+                    class="tooltip-set"
+                    title=""
+                  />
+                  &nbsp;
+                  <img
+                    alt="This job has just been posted and is fresh off the press"
+                    src="https://remoteok.com/assets/new2x.gif"
+                    width="30"
+                    height="14"
+                    class="tooltip-set"
+                    title=""
+                  ></img>
+                </span>
+                <br />
+                <div class="location">🌏 Worldwide</div>
+                <div class="location">💰 $40k - $50k</div>{" "}
+              </td>
+              <td class="tags">
+                <a
+                  class="no-border tooltip-set action-add-tag"
+                  data-tooltip="Add  to filters"
+                  href="/remote-content-writing-jobs"
+                  aria-label="Remote Content Writing Jobs"
+                  alt="Remote Content Writing Jobs"
+                >
+                  <div rel="follow" class="tag tag- action-add-tag">
+                    <h3>Content Writing </h3>
+                  </div>
+                  &nbsp;{" "}
+                </a>
+                <a
+                  class="no-border tooltip-set action-add-tag"
+                  data-tooltip="Add  to filters"
+                  href="/remote-non-tech-jobs"
+                  aria-label="Remote Non Tech Jobs"
+                  alt="Remote Non Tech Jobs"
+                >
+                  <div rel="follow" class="tag tag- action-add-tag">
+                    <h3>Non Tech </h3>
+                  </div>
+                  &nbsp;{" "}
+                </a>
+                <a
+                  class="no-border tooltip-set action-add-tag"
+                  data-tooltip="Add  to filters"
+                  href="/remote-writing-jobs"
+                  aria-label="Remote Writing Jobs"
+                  alt="Remote Writing Jobs"
+                >
+                  <div rel="follow" class="tag tag- action-add-tag">
+                    <h3>Writing </h3>
+                  </div>{" "}
+                </a>
+              </td>
+              <td class="time">
+                <time datetime="2023-10-05T00:44:12+00:00">
+                  <img
+                    src="https://remoteok.com/assets/paper-clip.svg"
+                    class="paper-clip"
+                  />
+                  28d{" "}
+                </time>
+              </td>
+              <td class="source">
+                <button type="button" class="btn btn-danger">
+                  Apply
+                </button>
+              </td>
+            </tr>
+            <tr class="items">
+              <td>
+                <a alt="IAPWE" class="preventLink" href="/">
+                  <img
+                    alt="IAPWE"
+                    data-z="1"
+                    data-extension="png"
+                    itemprop="image"
+                    class="logo lazy lazyloaded"
+                    src="https://remoteok.com/cdn-cgi/image/format=auto,fit=contain,width=100,height=100,quality=50/https://remoteOK.com/assets/img/jobs/f965d5c27e5954dbfffa10ecdbcd36331646841248.png?1646841248"
+                  />
+                </a>
+              </td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </container>
+    </div>
+  );
+}
+
+export default App;
