6308db23339a74c2439a91106e2107d8d6103ee5
Nguyễn Ngọc Cương <cuongnguyen71195@gmail.com>
2024-05-10 14:32:52 +0700

    gsygha

--- src/jsx/components/login.js
+++ src/jsx/components/login.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+
 import './signup.css'
 const formatStringToCamelCase = str => {
     const splitted = str.split("-");
--- src/jsx/components/signup.js
+++ src/jsx/components/signup.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import './signup.css'
 const formatStringToCamelCase = str => {
     const splitted = str.split("-");
